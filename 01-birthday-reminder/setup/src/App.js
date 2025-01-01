586fa70e3bec1252830393bb86847e701ff2dd9c
srikar <srikar.code01@gmail.com>
2025-01-01 20:17:27 +0530

    birthday reminder project

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,28 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
+import './index.css';
 function App() {
-  return <h2>hello world</h2>;
+  const [person, setPerson] = useState(data.length)
+  const [disable, setDisable] = useState(false)
+  console.log(disable);
+  
+  
+  return (
+    <>
+      <div className="conatiner w-full min-h-screen flex items-center justify-center bg-zinc-800">
+        <div className="w-[500px] h-auto border border-white bg-white rounded-lg shadow-md shadow-gray-500 p-4 ">
+          {/* <img src="https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg" alt="hello world" /> */}
+          <div className="text-xl text-center p-3">{person} birthdays today</div>
+          {disable ? <div></div> : <List /> }
+          <div className="mt-4 w-full">
+            <button onClick={() => setDisable(true)} className="w-full text-center text-xl bg-zinc-800 rounded-lg text-white p-2">Clear All</button>
+          </div>
+
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App;
--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,9 +1,25 @@
-import React from 'react';
-
+import React from "react";
+import data from "./data";
 const List = () => {
   return (
     <>
-      <h2>list component</h2>
+      <div className="conatiner">
+        {data.map((person) => (
+          <div className="flex gap-3 items-center " key={person.id}>
+            <div className="w-[20%] h-[20%] rounded-full overflow-hidden p-2">
+              <img
+                className="w-full h-full rounded-full object-cover"
+                src={person.image}
+                alt="image of a person"
+              />
+            </div>
+            <div>
+              <h1 className="text-green-500">{person.name}</h1>
+              <h1>{person.age} years</h1>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
