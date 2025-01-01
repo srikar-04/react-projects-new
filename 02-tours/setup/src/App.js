e63eb3f3c73509ccfaec3e6515d8ed86501445b0
srikar <srikar.code01@gmail.com>
2025-01-01 20:41:38 +0530

    tailwind setup

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -5,7 +5,11 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+  return (
+    <>
+       <h2 className=''>Tours Project Setup</h2>
+    </>
+  )
 }
 
 export default App
