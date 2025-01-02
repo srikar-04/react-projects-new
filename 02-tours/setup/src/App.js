e040a9bfebe9dff9995d44392a5009c4b7e7ebbc
srikar <srikar.code01@gmail.com>
2025-01-02 20:39:22 +0530

    got data from function, added loading state

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -1,13 +1,31 @@
 import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
+import data from './data';
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+// const url = 'https://course-api.com/react-tours-project'
+// const url = 'https://cors-anywhere.herokuapp.com/https://course-api.com/react-tours-project'
 function App() {
+
+  const [loading, setLoading] = useState(false);
+  const [tourData, setTourData] = useState([])
+
+  const fetchToursData = () => {
+    const toursData = data();
+    console.log(toursData, 'data'); 
+    setTourData(tourData);
+  }
+
+  useEffect(() => {
+    fetchToursData();
+  }, [])
+  
   return (
     <>
-       <h2 className=''>Tours Project Setup</h2>
+      <div className="container w-full h-screen bg-zinc-600">
+        {loading ? <Loading/> : <h2 className='text-white'>Tours Project Setup</h2> }
+      </div>
     </>
   )
 }
