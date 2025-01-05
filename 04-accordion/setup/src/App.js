64ecd83c7f5d7154903ce65ed0aa0fff1f88e1c3
srikar <srikar.code01@gmail.com>
2025-01-05 17:03:10 +0530

    complete accordian project

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,10 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
+  const [info, setInfo] = useState([])
+
+  //  console.log(info); 
+  // id, info, title
+
+  useEffect(() => {
+  setInfo(data)
+  },[])
+  
   return (
     <>
-      <h2 className='text-blue-500'>accordion project setup</h2>;
+      <div className="component  w-full min-h-screen flex">
+        <div className='container min-h-auto shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)]'>
+          <h1 className='text-3xl font-semibold'>questions and answers about login</h1>
+          <div>
+            {info && info.map((item) => <SingleQuestion key={item.id} id={item.id} info={item.info} title={item.title} />)}
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,34 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({id, info, title}) => {
+
+  // const [ids, setIds] = useState([])
+  const [plus, setPlus] = useState(true)
+
+  const button = () => {
+    setPlus(prev => !prev)
+  }
+
+   return (
+    <>
+      <div onClick={() => button()} className='text-lg p-[18px] my-2 rounded shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)] font-semibold flex items-center justify-between flex-col'>
+        <div className='flex items-center justify-between w-full '>
+          <span>{title}</span>
+          {plus 
+          ? 
+            <AiOutlinePlus
+              // onClick={() => button()}
+              className='rounded-full bg-gray-300 p-1 text-red-500 text-2xl cursor-pointer' /> 
+          :  
+            <AiOutlineMinus 
+              // onClick={() => button()}
+              className='rounded-full bg-gray-300 p-1 text-red-500 text-2xl cursor-pointer'/> }
+        </div>
+          
+        {plus ? <div></div> : <div className='inline-block text-left text-gray-500 text-[16px]'>{info}</div> }
+      </div>
+    </>
+  )
 };
 
 export default Question;
