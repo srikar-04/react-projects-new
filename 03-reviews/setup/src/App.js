a2d96d7960434f82fd763da2e36db40833cb9214
srikar <srikar.code01@gmail.com>
2025-01-05 08:46:14 +0530

    ui setup without functionality

--- 03-reviews/setup/src/App.js
+++ 03-reviews/setup/src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import Review from './Review';
 function App() {
-  return <h2>reviews project setup</h2>;
+  return (
+    <>
+      <div className='w-full h-screen bg-zinc-00 flex flex-col items-center justify-center'>
+        <h1 className='text-2xl mb-14 underline underline-offset-8 select-none'>Our Reviews</h1>
+        <Review />
+      </div>
+    </>
+  )
 }
 
 export default App;
--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -3,7 +3,44 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  // console.log(people);
+  const [index, setIndex] = useState(0);
+  const {id, image, job, name, text} = people[index]
+  
+  const rightButton = () => {
+    console.log('clicked on right button', Math.random());
+  }
+
+  const leftButton = () => {
+    console.log('clicked on left button', Math.random());
+  }
+
+  return (
+    <>
+      <div className='container h-auto border flex flex-col items-center select-none'>
+        {/* image div */}
+        <div className='mt-4 cursor-pointer w-[100px] h-[100px] relative'>
+          <div className='w-full h-full bg-blue-500 rounded-full absolute'></div>
+          <img className='w-full h-full rounded-full object-cover absolute left-[-12px]' src={image} alt={name} />
+          <FaQuoteRight className='absolute left-[-4px] bg-blue-500 text-white p-1 rounded-full text-2xl' />
+        </div>
+        <h1 className='cursor-pointer text-[15px] mt-3 mb-2 tracking-tight'>{name}</h1>
+        <h1 className='cursor-pointer text-sm text-blue-400 mb-2'>{job}</h1>
+        <p className='leading-6 text-sm text-center mx-10'>{text}</p>
+        {/* buttons */}
+        <div className='flex text-blue-500 cursor-pointer mb-3'>
+          <FaChevronLeft
+            onClick={() => leftButton()}
+            className='hover:text-blue-700' />
+          <FaChevronRight
+            onClick={() => rightButton()}
+            className='hover:text-blue-700' />
+        </div>
+        {/* surprise button */}
+        <button className='random-btn mb-3'>surprise me</button>
+      </div>
+    </>
+  )
 };
 
 export default Review;
