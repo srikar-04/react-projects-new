9c02d1eb9f62c67bf843b4a41232284b811c1645
srikar <srikar.code01@gmail.com>
2025-01-05 10:47:25 +0530

    taiwind setup

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -2,7 +2,11 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  return (
+    <>
+      <h2 className='text-blue-500'>accordion project setup</h2>;
+    </>
+  )
 }
 
 export default App;
